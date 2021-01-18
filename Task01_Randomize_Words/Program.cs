e6e746ad9e59addadcca2aca9508613b303a2176
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-18 22:48:12 +0200

    Add files via upload

---
+++ Task01_Randomize_Words/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Task01_Randomize_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            Random rnd = new Random();
+            for (int poitionOne = 0; poitionOne < words.Length; poitionOne++)
+            {
+                int positionTwo = rnd.Next(words.Length);
+                string helpVar = words[poitionOne];
+                words[poitionOne] = words[positionTwo];
+                words[positionTwo] = helpVar;
+            }
+            
+            Console.WriteLine(string.Join((char)10, words));
+        }
+    }
+}
