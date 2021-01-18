7e70fbd2b48c2deaa5a4afb9bb2131926193ab82
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-18 22:49:18 +0200

    Add files via upload

---
+++ Task02_Big_Factorial/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+namespace Task02_Big_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            BigInteger nFactorial = 1;
+
+            for (int i = 2; i <= n; i++)
+            {
+                nFactorial *= i;
+            }
+            
+            Console.WriteLine(nFactorial);
+        }
+    }
+}
